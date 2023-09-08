7e0770aa7117258c56a8c3aee793b60896c314ee
CristianWhitesoft <cristian.badillo@whitesoft.com.au>
2023-09-08 01:04:28 -0500

    JWT_SECRET

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -5,7 +5,6 @@ import {
     UnauthorizedException,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { jwtConstants } from './constants';
 import { Request } from 'express';
 
 @Injectable()
@@ -19,13 +18,8 @@ export class AuthGuard implements CanActivate {
             throw new UnauthorizedException();
         }
         try {
-            const payload = await this.jwtService.verifyAsync(
-                token,
-                {
-                    secret: jwtConstants.secret
-                }
-            );
-            request['user'] = payload;
+            const payload = await this.jwtService.verifyAsync(token);
+            request.user = payload;
         } catch {
             throw new UnauthorizedException();
         }
